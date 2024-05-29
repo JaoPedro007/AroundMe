a3d982148ebc959b95f29f8f6ddef585da630673
JaoPedro007 <joaopedrorodriguesleite9@gmail.com>
2024-05-29 00:17:11 +0000

    Change width and height of the logo

--- src/pages/components/logo/index.js
+++ src/pages/components/logo/index.js
@@ -3,6 +3,9 @@ import { Image } from "react-native";
 export function Logo(){
     return (
     <Image
+        style={{
+        width: 350,
+        height: 350}}
         source = {require('../../../assets/logo/logo.png')}
       />
 
